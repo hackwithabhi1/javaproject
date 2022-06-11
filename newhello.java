394dc3927e58dcc4622a4295743de11f3a66fdbd
sachin <sachin@gmail.com>
2022-06-11 15:21:39 +0530

    new for punkear

--- newhello.java
+++ newhello.java
@@ -5,7 +5,7 @@ class forLoopDemo {
         // Writing a for loop
         // to print Hello World 5 times
         for (int i = 1; i <= 10; i++)
-            System.out.println("Good Morning Pune!");
+            System.out.println("Good Morning Punekar!");
     }
 }
 
